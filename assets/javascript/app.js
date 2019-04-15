7995edf6141b7acef7a171e8fb618cfc97837671
Alex <suarez9093@hotmail.com>
2019-04-14 23:35:47 -0600

    final working trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,20 +27,17 @@ var questions = [
         correctAnswer: "grandma"
     }
 ]
-
- // Hides the submitButton
- $("#submitButton").hide();
- // Try again button hide at start
- $("#tryAgainButton").hide();
+// Hides the submitButton
+$("#submitButton").hide();
+// Try again button hide at start
+$("#tryAgainButton").hide();
 var game = {
-
     // right answer tracker
     correctAnswers: 0,
     // wrong answer tracker
     wrongAnswers: 0,
-
-    counter: 30,
-
+    // Clock counter
+    counter: 15,
 
     // Functions
     // ==========================================================================================================
@@ -56,20 +53,14 @@ var game = {
         $("#submitButton").hide();
         // Try again button hide at start
         $("#tryAgainButton").hide();
-        // $("#tryAgainButton").on("click", startTrivia());
-
-        // ---------------------------------------------------------------------------------------------
+        // Loop that displays questions
         for (var i = 0; i < questions.length; i++) {
             // Append the questions
             $(".questions").append("<h3>" + questions[i].question + "</h3>");
-
             for (var j = 0; j < questions[i].answers.length; j++) {
                 $(".questions").append("<br>" + " <input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'' > " + questions[i].answers[j])
             }
-
-
         }
-        // ----------------------------------------------------------------------------------------------------------------------------------
         // Show the questions
         $(".questions").show();
         // Show the submitButton
@@ -83,25 +74,57 @@ var game = {
         // wrongAnswerSummary
         $("#wrongAnswerSummary").hide();
         // to prevent multiple intervals being set simultaneously
-            intervalID = setInterval(game.count, 1000);
-            // only have one running interval at a time
-            clockRunning = true;
-
-       
+        intervalID = setInterval(game.count, 1000);
+        // only have one running interval at a time
+        clockRunning = true;
     },
-    // starts the startTrivia function 
-    // startTrivia();
 
     // Reset
     reset: function () {
-        // $("#timeRemaining").hide();
-        $(".questions").hide();
-        // Hides the submit button
-        $("#submitButton").hide();
-        // Shows the try again button
-        $("#tryAgainButton").show();
-        // Times up text
-        $("#timeRemaining").text("Time's up!");
+        // Stops the count 
+        clearInterval(intervalID);
+        // Sets the clock to not running
+        clockRunning = false;
+        // resets the time
+        game.counter = 15;
+        // resets correct answers
+        game.correctAnswers = 0;
+        // resets wrong answers
+        game.wrongAnswers = 0;
+        // questions
+        $(".questions").empty();
+    },
+    // Holds the count
+    count: function () {
+        game.counter--;
+        $("#timeRemaining").html("<h3>Time Remaining: " + game.counter + " seconds </h3>");
+        if (game.counter === 0) {
+            alert("Time is up!");
+            game.checkAnswer();
+        }
+    },
+    // Gets the answers chosen and scores them based on right and wrond answers
+    checkAnswer: function () {
+        $.each($("input[name=question-0]:checked"), function () {
+            if ($(this).val() === questions[0].correctAnswer) {
+                game.correctAnswers++;
+            } else { game.wrongAnswers++; }
+        });
+        $.each($("input[name=question-1]:checked"), function () {
+            if ($(this).val() === questions[1].correctAnswer) {
+                game.correctAnswers++;
+            } else { game.wrongAnswers++; }
+        });
+        $.each($("input[name=question-2]:checked"), function () {
+            if ($(this).val() === questions[2].correctAnswer) {
+                game.correctAnswers++;
+            } else { game.wrongAnswers++; }
+        });
+        $.each($("input[name=question-3]:checked"), function () {
+            if ($(this).val() === questions[3].correctAnswer) {
+                game.correctAnswers++;
+            } else { game.wrongAnswers++; }
+        });
         // show the correctAnswerSummary
         $("#correctAnswerSummary").show();
         // show the wrongAnswerSummary
@@ -110,75 +133,18 @@ var game = {
         $("#correctAnswerSummary").text("Correct Answers: " + game.correctAnswers);
         // wrongAnswerSummary
         $("#wrongAnswerSummary").text("Wrong Answers: " + game.wrongAnswers);
-        // Stops the count 
-        clearInterval(intervalID);
-        // Sets the clock to not running
-        clockRunning = false;
-        // resets the time
-        game.counter = 30;
-
-        console.log(intervalID);
+        // Shows the try again button
+        $("#tryAgainButton").show();
+        // Times up text
+        $("#timeRemaining").text("Time's up!");
+        $(".questions").hide();
+        // Hides the submit button
+        $("#submitButton").hide();
     },
 
-
-    // Holds the count
-    count: function () {
-        game.counter--;
-
-        $("#timeRemaining").html("<h3>Time Remaining: " + game.counter + " seconds </h3>");
-        if (game.counter === 0) {
-            alert("Time is up!")
-            game.reset();
-           
-        }
-    
-    },
-
-
-    // Gets the answers chosen and scores them based on right and wrond answers
-    // --------------------------------------------------------------------------------------------------------------------------------
-    checkAnswer: function () {
-        $.each($("input[name=question-0]:checked"), function(){
-            if ($(this).val() === questions[0].correctAnswer){
-                        game.correctAnswers++;
-                    } else {game.wrongAnswers++;} 
-                   
-        });
-
-        $.each($("input[name=question-1]:checked"), function(){
-            if ($(this).val() === questions[1].correctAnswer){
-                        game.correctAnswers++;
-                    } else {game.wrongAnswers++;} 
-                   
-        });
-
-        $.each($("input[name=question-2]:checked"), function(){
-            if ($(this).val() === questions[2].correctAnswer){
-                        game.correctAnswers++;
-                    } else {game.wrongAnswers++;} 
-                   
-        });
-
-        $.each($("input[name=question-3]:checked"), function(){
-            if ($(this).val() === questions[3].correctAnswer){
-                        game.correctAnswers++;
-                    } else {game.wrongAnswers++;} 
-                   
-        });
-
-        console.log(game.correctAnswers)
-        console.log(game.wrongAnswers)
-      
-
-        
-
-        game.reset();
-    },
-   
 
     // When you click the submit button
     submit: function () {
-        $("#submitButton").on("click", function () {
             // Stops the count 
             clearInterval(intervalID);
             // Sets the clock to not running
@@ -199,34 +165,21 @@ var game = {
             $("#tryAgainButton").show();
             // Hides the submitButton
             $("#submitButton").hide();
-            reset();
-
-        });
-
     }
-
-
-
-
 }
 
-$("#startButton").on("click" , function(){
+$("#startButton").on("click", function () {
     $("#timeRemaining").show();
     game.startTrivia();
-})
+});
 
-$("#submitButton").on("click" , function(){
+$("#submitButton").on("click", function () {
     game.checkAnswer();
     game.reset();
-})
+});
 
-$("#tryAgainButton").on("click" , function(){
+$("#tryAgainButton").on("click", function () {
     $("#timeRemaining").show();
-
+    game.reset();
     game.startTrivia();
-})
-
-
-
-
-
+});
